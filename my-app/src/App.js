fff379a15946ea59ab8333c8c2ab5d603ce56e55
Ayuure <bertinaayuure0147@gmail.com>
2022-11-21 15:27:29 +0000

    props completed, styling

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './App.css';
 import Contact from './Contact';
-import rama from './images/ama.jpg'
+import rama from './images/ama.jpg';
+import titi from './images/titi.jpg';
+import sefakor from './images/sefakor.jpg';
+import bertina from './images/bertina.jpg';
 
 
 function App() {
   return (
     <div>
       <Contact
-      img={"./images/ama.jpg"}
+      img={rama}
       name="Ewurama Amponsah"
       phone="+233 088945 309390"
       email="titi@gmail.com"
@@ -16,26 +19,26 @@ function App() {
       />
       
       <Contact
-img="../images/ama.jpg"
-name=" Multiti Bilal"
-phone="+233 088945 309390"
-email="titi@gmail.com"
+        img={titi}
+        name=" Multiti Bilal"
+        phone="+233 088945 309390"
+        email="titi@gmail.com"
 />
-<Contact
-img="./images/sefakor.jpg"
-name="Sefakor Dodor"
-phone="+233 088945 309390"
-email="sefakor@gamil.com"
+  <Contact
+    img={sefakor}
+    name="Sefakor Dodor"
+    phone="+233 088945 309390"
+    email="sefakor@gamil.com"
 />
-<Contact
-img="./images/bertina.jpg"
-name="Bertina"
-phone="+233 088945 309390"
-email=" bertina@gmail.com"
+  <Contact
+    img={bertina}
+    name="Bertina"
+    phone="+233 088945 309390"
+    email=" bertina@gmail.com"
 />
   
        
-        </div>
+</div>
   )
    
 }
